9210381fb82b71b69bf2a56346e0e77b35d63ff1
Martí SB <50743560+martisalcedo7@users.noreply.github.com>
2022-09-06 15:22:18 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,170 +1,90 @@
 import numpy as np
+import sys
+import pygame
+import argparse
+from robot.robot import Robot
 
-
-class Robot():
-    
-    def __init__(self, origin):
-        self.x_0 = origin
-        self.link_length = np.array([80, 80, 80])
-        self.w = np.matrix([[1.0,0.0,0.0],[0.0,1.0,0.0],[0.0,0.0,1.0]])
-        self.w_inv = np.linalg.inv(self.w)
-
-    def forward_kinematics(self, thetas):
-        x_1 = self.x_0[0] + self.link_length[0]*np.cos(thetas[0,0])
-        y_1 = self.x_0[1] + self.link_length[0]*np.sin(thetas[0,0])
-        x_2 = x_1 + self.link_length[1]*np.cos(thetas[0,0] + thetas[1,0])
-        y_2 = y_1 + self.link_length[1]*np.sin(thetas[0,0] + thetas[1,0])
-        x_3 = x_2 + self.link_length[2]*np.cos(thetas[0,0] + thetas[1,0] + thetas[2,0])
-        y_3 = y_2 + self.link_length[2]*np.sin(thetas[0,0] + thetas[1,0] + thetas[2,0])
-
-        return np.array([self.x_0, [x_1, y_1], [x_2, y_2], [x_3, y_3]])
-    
-    def jacobian(self, thetas):
-        jx_3 = -self.link_length[2]*np.sin(thetas[0,0]+thetas[1,0]+thetas[2,0])
-        jx_2 = jx_3-self.link_length[1]*np.sin(thetas[0,0] + thetas[1,0])
-        jx_1 = jx_2-self.link_length[0]*np.sin(thetas[0,0])
-        jy_3 = self.link_length[2]*np.cos(thetas[0,0]+thetas[1,0]+thetas[2,0])
-        jy_2 = jy_3+self.link_length[1]*np.cos(thetas[0,0] + thetas[1,0])
-        jy_1 = jy_2+self.link_length[0]*np.cos(thetas[0,0])
-        
-        return np.matrix([[jx_1,jx_2,jx_3], 
-                        [jy_1,jy_2,jy_3]])
-    
-    def inverse_kinematics(self, x_d, q):
-
-        if np.linalg.norm(x_d) > np.sum(self.link_length):
-            print("Position out of limits")
-            return q
-
-        x_current = np.transpose(np.matrix(self.forward_kinematics(q)[3]))
-        x_inc = x_d - x_current 
-
-        counter = 0
-        while np.linalg.norm(x_inc) > 0.001:
-
-            j = self.jacobian(q)
-            j_t = np.transpose(j)
-            pseudo_j = self.w_inv * j_t * np.linalg.inv(j * self.w_inv * j_t)
-
-            q_inc = pseudo_j * x_inc
-            q = q + q_inc
-            x_current = np.transpose(np.matrix(self.forward_kinematics(q)[3]))
-            x_inc =  x_d - x_current
-
-            if counter > 100:
-                break
-
-            counter += 1
-        
-        return q
-    
-    def inverse_kinematics_null_space(self, x_d, q, q_star):
-
-        if np.linalg.norm(x_d) > np.sum(self.link_length):
-            print("Position out of limits")
-            return q
-
-        x_current = np.transpose(np.matrix(self.forward_kinematics(q)[3]))
-        x_inc = x_d - x_current
-
-        alpha = 0.5
-
-        counter = 0
-        while np.linalg.norm(x_inc) > 0.001:
-
-            j = self.jacobian(q)
-            j_t = np.transpose(j)
-            pseudo_j = self.w_inv * j_t * np.linalg.inv(j * self.w_inv * j_t)
-            Nw = np.identity(3) - pseudo_j * j
-
-            q_inc = pseudo_j * x_inc - alpha * Nw * self.w_inv * (q - q_star)
-            q = q + q_inc
-            x_current = np.transpose(np.matrix(self.forward_kinematics(q)[3]))
-            x_inc =  x_d - x_current
-
-            if counter > 100:
-                break
-
-            counter += 1
-        
-        return q
-
-    def inverse_kinematics_transpose(self, x_d, q):
-
-        if np.linalg.norm(x_d) > np.sum(self.link_length):
-            print("Position out of limits")
-            return q
-
-        x_current = np.transpose(np.matrix(self.forward_kinematics(q)[3]))
-        x_inc = x_d - x_current 
-
-        counter = 0
-        while np.linalg.norm(x_inc) > 0.001:
-
-            j = self.jacobian(q)
-            j_t = np.transpose(j)
-
-            q_inc = j_t * x_inc * 0.00001
-            q = q + q_inc
-            x_current = np.transpose(np.matrix(self.forward_kinematics(q)[3]))
-            x_inc =  x_d - x_current
-
-            if counter > 100:
-                break
-
-            counter += 1
-        
-        return q        
-        
-        
 
 def main():
-    import sys, pygame
+    # Parse arguments
+    parser = argparse.ArgumentParser(
+        description='2D Robotic arm inverse kinematics.')
+
+    parser.add_argument('-m', '--method', dest='method', default='n', required=False, choices='ptn',
+                        help='Method to calculate inverse kinematics: p->pseude transpose, t->transpose, n->null space')
+    args = parser.parse_args()
+
+    # Init pygame
     pygame.init()
+    # Create screen
     size = width, height = 640, 480
+    pygame.display.set_caption('2D redundant robotic arm')
+    screen = pygame.display.set_mode(size)
+    # Define colors
     black = (0, 0, 0)
     red = (255, 0, 0)
     green = (0, 255, 0)
     blue = (0, 0, 255)
-    screen = pygame.display.set_mode(size)
 
-    # Robot stuff
+    # Robot
+    # Define robot origin
     origin = np.array([0.0, 0.0])
+    # Load Robot object
     robot = Robot(origin)
+    # Define robot initial position
     q_pos = np.matrix([[0.1], [0.3], [0.2]])
+    # Defined robot default position for the null-state solver
     q_star = np.matrix([[np.pi/2.0], [0], [np.pi/2.0]])
-    
+
+    # Start infinite loop
     while True:
+
+        # FPS control
         start_time = pygame.time.get_ticks()
+
+        # Events handler
         for event in pygame.event.get():
-            if event.type == pygame.QUIT: sys.exit()
-        
-        # Robot stuff
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        # Capture mouse position
         x_mouse, y_mouse = pygame.mouse.get_pos()
+
+        # Transform mouse position to robot coordinates frame
         x_mouse = x_mouse - width/2.0
         y_mouse = height/2.0 - y_mouse
 
-        # print(x_mouse, y_mouse)
+        # Compute robot inverse kinematics
+        if args.method == 'p':
+            q_pos = robot.inverse_kinematics_pseudo_inverse(
+                np.matrix([[x_mouse], [y_mouse]]), q_pos)
+        elif args.method == 't':
+            q_pos = robot.inverse_kinematics_transpose(
+                np.matrix([[x_mouse], [y_mouse]]), q_pos)
+        else:
+            q_pos = robot.inverse_kinematics_null_space(
+                np.matrix([[x_mouse], [y_mouse]]), q_pos, q_star)
 
-        q_pos = robot.inverse_kinematics_null_space(np.matrix([[x_mouse],[y_mouse]]), q_pos, q_star)
+        # Compute robot forward kinematics
         x_pos = robot.forward_kinematics(q_pos)
 
-        print(q_pos)
+        # Transform robot position to pygame coordinates frame
+        x_pos[:, 0] += width/2.0
+        x_pos[:, 1] += height/2.0
+        x_pos[0:, 1] = height - x_pos[0:, 1]
 
-        #Coordinates adjustment
-        x_pos[:,0] += width/2.0
-        x_pos[:,1] += height/2.0
-        x_pos[0:,1] = height - x_pos[0:,1]
-        #
-
+        # Clear screen
         screen.fill(black)
+        # Draw lines
         pygame.draw.line(screen, red, x_pos[0], x_pos[1])
         pygame.draw.line(screen, green, x_pos[1], x_pos[2])
         pygame.draw.line(screen, blue, x_pos[2], x_pos[3])
+        # Show drawn objects
         pygame.display.flip()
 
+        # FPS control
         end_time = pygame.time.get_ticks()
-        pygame.time.wait(20 - (end_time - start_time) ) #50fps
+        pygame.time.wait(20 - (end_time - start_time))  # 50fps
 
 
 if __name__ == "__main__":
---
+++ robot/robot.py
@@ -0,0 +1,127 @@
+import numpy as np
+
+
+class Robot():
+
+    def __init__(self, origin):
+        self.x_0 = origin
+        self.link_length = np.array([80, 80, 80])
+        self.w = np.matrix([[1.0, 0.0, 0.0], [0.0, 1.0, 0.0], [0.0, 0.0, 1.0]])
+        self.w_inv = np.linalg.inv(self.w)
+
+    def forward_kinematics(self, thetas):
+        x_1 = self.x_0[0] + self.link_length[0]*np.cos(thetas[0, 0])
+        y_1 = self.x_0[1] + self.link_length[0]*np.sin(thetas[0, 0])
+        x_2 = x_1 + self.link_length[1]*np.cos(thetas[0, 0] + thetas[1, 0])
+        y_2 = y_1 + self.link_length[1]*np.sin(thetas[0, 0] + thetas[1, 0])
+        x_3 = x_2 + self.link_length[2] * \
+            np.cos(thetas[0, 0] + thetas[1, 0] + thetas[2, 0])
+        y_3 = y_2 + self.link_length[2] * \
+            np.sin(thetas[0, 0] + thetas[1, 0] + thetas[2, 0])
+
+        return np.array([self.x_0, [x_1, y_1], [x_2, y_2], [x_3, y_3]])
+
+    def jacobian(self, thetas):
+        jx_3 = -self.link_length[2] * \
+            np.sin(thetas[0, 0]+thetas[1, 0]+thetas[2, 0])
+        jx_2 = jx_3-self.link_length[1]*np.sin(thetas[0, 0] + thetas[1, 0])
+        jx_1 = jx_2-self.link_length[0]*np.sin(thetas[0, 0])
+        jy_3 = self.link_length[2] * \
+            np.cos(thetas[0, 0]+thetas[1, 0]+thetas[2, 0])
+        jy_2 = jy_3+self.link_length[1]*np.cos(thetas[0, 0] + thetas[1, 0])
+        jy_1 = jy_2+self.link_length[0]*np.cos(thetas[0, 0])
+
+        return np.matrix([[jx_1, jx_2, jx_3],
+                          [jy_1, jy_2, jy_3]])
+
+    def inverse_kinematics_pseudo_inverse(self, x_d, q):
+
+        if np.linalg.norm(x_d) > np.sum(self.link_length):
+            print("Position out of limits")
+            return q
+
+        x_current = np.transpose(np.matrix(self.forward_kinematics(q)[3]))
+        x_inc = x_d - x_current
+
+        counter = 0
+        while np.linalg.norm(x_inc) > 0.001:
+
+            j = self.jacobian(q)
+            j_t = np.transpose(j)
+            pseudo_j = self.w_inv * j_t * np.linalg.inv(j * self.w_inv * j_t)
+
+            q_inc = pseudo_j * x_inc
+            q = q + q_inc
+            x_current = np.transpose(np.matrix(self.forward_kinematics(q)[3]))
+            x_inc = x_d - x_current
+
+            if counter > 100:
+                break
+
+            counter += 1
+
+        print(q)
+
+        return q
+
+    def inverse_kinematics_null_space(self, x_d, q, q_star):
+
+        if np.linalg.norm(x_d) > np.sum(self.link_length):
+            print("Position out of limits")
+            return q
+
+        x_current = np.transpose(np.matrix(self.forward_kinematics(q)[3]))
+        x_inc = x_d - x_current
+
+        alpha = 0.5
+
+        counter = 0
+        while np.linalg.norm(x_inc) > 0.0001:
+
+            j = self.jacobian(q)
+            j_t = np.transpose(j)
+            pseudo_j = self.w_inv * j_t * np.linalg.inv(j * self.w_inv * j_t)
+            Nw = np.identity(3) - pseudo_j * j
+
+            q_inc = pseudo_j * x_inc - alpha * Nw * self.w_inv * (q - q_star)
+            q = q + q_inc
+            x_current = np.transpose(np.matrix(self.forward_kinematics(q)[3]))
+            x_inc = x_d - x_current
+
+            if counter > 100:
+                break
+
+            counter += 1
+
+        print(q)
+
+        return q
+
+    def inverse_kinematics_transpose(self, x_d, q):
+
+        if np.linalg.norm(x_d) > np.sum(self.link_length):
+            print("Position out of limits")
+            return q
+
+        x_current = np.transpose(np.matrix(self.forward_kinematics(q)[3]))
+        x_inc = x_d - x_current
+
+        counter = 0
+        while np.linalg.norm(x_inc) > 0.001:
+
+            j = self.jacobian(q)
+            j_t = np.transpose(j)
+
+            q_inc = j_t * x_inc * 0.00001
+            q = q + q_inc
+            x_current = np.transpose(np.matrix(self.forward_kinematics(q)[3]))
+            x_inc = x_d - x_current
+
+            if counter > 100:
+                break
+
+            counter += 1
+
+        print(q)
+
+        return q
